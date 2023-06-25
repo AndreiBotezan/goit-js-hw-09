96edf29f35a7b7b7d78dabf82dfb2297e7cd4651
AndreiBotezan <73821849+AndreiBotezan@users.noreply.github.com>
2023-06-25 15:56:47 +0000

    Deploying to gh-pages from @ AndreiBotezan/goit-js-hw-09@b11dc85408e1d87f2b2111c95fdf94fdb262200e 🚀

---
+++ 01-color-switcher.bff92ee6.js
@@ -0,0 +1,2 @@
+let e;const t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");d.disabled=!0,t.addEventListener("click",function(){e=setInterval(()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`},1e3),t.disabled=!0,d.disabled=!1}),d.addEventListener("click",function(){t.disabled=!1,clearInterval(e),t.disabled=!1,d.disabled=!0});
+//# sourceMappingURL=01-color-switcher.bff92ee6.js.map
