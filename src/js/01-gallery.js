e494b80a15de9acbe5c0c04da57e71295f61f698
AndreiBotezan <andreibotezan@gmail.com>
2023-06-25 18:39:14 +0300

    first commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items';
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const makeGalleryCard = ({ preview, original, description }) =>
+  `<a class="gallery__link" href="${original}">
+     <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+
+const markup = galleryItems.map(element => makeGalleryCard(element)).join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', markup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
